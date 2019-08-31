8e8bc36c92860e6eb2bf3dd1ad19d68e4c741c4f
hidemune <auau@kne.biglobe.ne.jp>
2019-08-31 22:51:32 +0900

    Debug Signed-off-by: hidemune <auau@kne.biglobe.ne.jp>

--- usbList.sh
+++ usbList.sh
@@ -8,7 +8,7 @@ mount -o iocharset=utf8 /dev/${DEVICE} /home/pi/mount/
 
 if [ $? -gt 0 ] ; then
   echo No USB...
-  find /home/pi/Videos/ | sort > work.txt
+  find /home/pi/Videos/ -type f | sort > work.txt
 
   if diff -q work.txt bkup.txt >/dev/null ; then
     sudo ./getIP_Nightly.sh
@@ -25,7 +25,7 @@ fi
 
 lsblk -n -o NAME,MOUNTPOINT
 
-find $(lsblk -n -o MOUNTPOINT /dev/${DEVICE}) | sort > work.txt
+find $(lsblk -n -o MOUNTPOINT /dev/${DEVICE}) -type f | sort > work.txt
 
 if diff -q work.txt bkup.txt >/dev/null ; then
   sudo ./getIP_Nightly.sh
