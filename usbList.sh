7b256b125e9f6da2d1b18367f2d3bf1387f98ff4
hidemune <auau@kne.biglobe.ne.jp>
2019-08-31 13:24:12 +0900

    Debug Signed-off-by: hidemune <auau@kne.biglobe.ne.jp>

--- usbList.sh
+++ usbList.sh
@@ -2,7 +2,20 @@
 
 cd /home/pi/git/RaspiFlacPlayer/
 
-find $(lsblk -n -o MOUNTPOINT /dev/sda1) | sort > work.txt
+DEVICE=sda1
+
+mount /dev/${DEVICE} /home/pi/mount/
+
+if [ $? -gt 0 ] ; then
+  echo No USB...
+  ./makeCSV.sh /home/pi/Videos/
+  sudo ./getIP_Nightly.sh
+  exit 0
+fi
+
+lsblk -n -o NAME,MOUNTPOINT
+
+find $(lsblk -n -o MOUNTPOINT /dev/${DEVICE}) | sort > work.txt
 
 if diff -q work.txt bkup.txt >/dev/null ; then
   sudo ./getIP_Nightly.sh
