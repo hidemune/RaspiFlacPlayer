ad895231a10abf509600a02f4765de3820560210
hidemune <auau@kne.biglobe.ne.jp>
2019-09-07 19:30:33 +0900

    Debug Signed-off-by: hidemune <auau@kne.biglobe.ne.jp>

--- getIP_Nightly.sh
+++ getIP_Nightly.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-echo "開始します。" > url.txt
-open_jtalk -x /var/lib/mecab/dic/open-jtalk/naist-jdic -m /usr/share/hts-voice/nitech-jp-atr503-m001/nitech_jp_atr503_m001.htsvoice -r 1.0 -ow url.wav url.txt
-aplay url.wav
-
 cd /home/pi/
 bash start-ap-management-wifi.sh
 sleep 10
