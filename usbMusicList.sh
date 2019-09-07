207c5fb55b66582eb2cb560d517e18a619ad58f0
TANAKA Hidemune <auau@kne.biglobe.ne.jp>
2019-09-07 17:40:10 +0900

    Debug Signed-off-by: TANAKA Hidemune <auau@kne.biglobe.ne.jp>

--- usbMusicList.sh
+++ usbMusicList.sh
@@ -29,6 +29,7 @@ fi
 lsblk -n -o NAME,MOUNTPOINT
 
 sudo cp -f $(lsblk -n -o MOUNTPOINT /dev/${DEVICE})/playerSetting/bkup.txt ./bkup.txt
+sudo cp -f $(lsblk -n -o MOUNTPOINT /dev/${DEVICE})/playerSetting/*.csv ./
 find $(lsblk -n -o MOUNTPOINT /dev/${DEVICE}) -type d -name playerSetting -prune -o -type f | sort > work.txt
 
 if diff -q work.txt bkup.txt >/dev/null ; then
@@ -37,8 +38,9 @@ else
   # Diff!
   sudo cp -f work.txt bkup.txt
   sudo mkdir $(lsblk -n -o MOUNTPOINT /dev/${DEVICE})/playerSetting/
-  sudo cp -f bkup.txt $(lsblk -n -o MOUNTPOINT /dev/${DEVICE})/playerSetting/bkup.txt
   ./makeCSV.sh $(lsblk -n -o MOUNTPOINT /dev/${DEVICE})
+  sudo cp -f *.csv $(lsblk -n -o MOUNTPOINT /dev/${DEVICE})/playerSetting/
+  sudo cp -f bkup.txt $(lsblk -n -o MOUNTPOINT /dev/${DEVICE})/playerSetting/bkup.txt
   sudo ./getIP_Nightly.sh
   sudo umount $(lsblk -n -o MOUNTPOINT /dev/${DEVICE})
 fi
